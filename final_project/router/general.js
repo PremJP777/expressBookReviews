f1fbbc545d9462880d4ea6a4e5413cd811a03055
PREM SAGAR JONNALAGADDA <premsagar.jonnalagadda@gmail.com>
2024-06-25 02:29:03 +0530

    Task - 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -52,7 +52,8 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn=parseInt(req.params.isbn);
+  res.send(JSON.stringify(books[isbn].reviews,null,4));
 });
 
 module.exports.general = public_users;
