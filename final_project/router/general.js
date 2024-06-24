70b059796abfa6abcf038156fda54e7beb76461a
PREM SAGAR JONNALAGADDA <premsagar.jonnalagadda@gmail.com>
2024-06-25 02:27:30 +0530

    Task - 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,7 +26,14 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author=req.params.author;
+  let samp_books=[];
+  for(item in books){
+    if(books[item].author==author){
+        res.send(JSON.stringify(books[item],null,4));
+    }
+  }
+  res.send("Sorry the requested book cannot be found");
 });
 
 // Get all books based on title
